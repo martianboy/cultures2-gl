dd8f95e9716621e0c3d8be7dba7d9bd628b5c67a
Abbas Mashayekh <martianboy2005@gmail.com>
2020-03-20 23:16:58 +0330

    Fix bmd shadow alpha blending mode

--- src/game/map/index.ts
+++ src/game/map/index.ts
@@ -39,7 +39,7 @@ export class CulturesMap {
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
     this.gl.enable(this.gl.BLEND);
-    this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
+    this.gl.blendFunc(this.gl.ONE, this.gl.ONE_MINUS_SRC_ALPHA);
     // this.gl.blendEquationSeparate(this.gl.FUNC_ADD, this.gl.FUNC_ADD);
     // this.gl.blendFuncSeparate(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA, this.gl.ONE, this.gl.ZERO);
 
